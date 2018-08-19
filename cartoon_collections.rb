bdc92b56cffc91a97c4f9978755af31ea3e054fc
Sam Ashtar <samashtarm@gmail.com>
2018-08-19 23:09:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls (call)
   
 end 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese (array)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
